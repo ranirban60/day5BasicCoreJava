108ff61ecfe50d46d86dad193382f97978d3aab5
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-25 21:12:34 +0530

    Added Program Harmonic Number

---
+++ src/main/java/com/bridgelabz/HarmonicNumber.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class HarmonicNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        double n = sc.nextDouble();
+        double result = harmonicValue(n);
+
+        if(n>0){
+            System.out.println("The Harmonic value for the number is:" +result);
+        }
+        else{
+            System.out.println("The number should be greater than zero" );
+        }
+    }
+
+    private static double harmonicValue(double n) {
+        double val = 0.0;
+     for (int i =1; i<=n; i++){
+         val = val + (1.0/i);
+     }
+     return val;
+    }
+}
