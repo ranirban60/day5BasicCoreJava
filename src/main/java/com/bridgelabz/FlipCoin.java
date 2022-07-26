1afb1a088b59f5c9c40928fee7475e1d6751ff53
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-26 21:26:35 +0530

    Added the program Flip a Coin

--- src/main/java/com/bridgelabz/FlipCoin.java
+++ src/main/java/com/bridgelabz/FlipCoin.java
@@ -1,13 +1,32 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class FlipCoin {
     public static void main(String[] args) {
+            int head=0;
+            int tail=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number of Flipping coin n ");
 
-            if (Math.random() < 0.5) {
+        int n = sc.nextInt();
+           for(int i=0;i<n; i++){
+          double randomValue = Math.random();
+          System.out.println("Random Value " +randomValue);
+          if (randomValue < 0.5) {
+              head++;
                 System.out.println("Head");
             } else {
+              tail++;
                 System.out.println("Tail");
             }
         }
+           System.out.println("Number of Head is"+" " +head);
+           System.out.println("Number of Tail is"+" " +tail);
+           int percentageOfHead = ((head *100)/n);
+           System.out.println("Percentage of Head is" + " "+percentageOfHead);
+           int percentageOfTail = ((tail *100)/n);
+           System.out.println("Percentage of Tail is" + " "+percentageOfTail);
+    }
     }
 
