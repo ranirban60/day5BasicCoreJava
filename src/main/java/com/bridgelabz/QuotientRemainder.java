6c185e7ecf7b1fc42ae9f48501c9b7331393c47d
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-26 09:40:58 +0530

    Added Program to find Quotient and Remainder

---
+++ src/main/java/com/bridgelabz/QuotientRemainder.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Dividend");
+        int dividend = sc.nextInt();
+        System.out.println("Enter the Divisor");
+        int divisor = sc.nextInt();
+
+        int quotient = dividend/divisor;
+        int remainder = dividend%divisor;
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
