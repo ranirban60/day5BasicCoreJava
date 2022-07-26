d2eb03d9b365ba4224bdf8b3fb2949630629e6ed
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-26 10:12:15 +0530

    Added Program to check Vowel or Consonant

---
+++ src/main/java/com/bridgelabz/VowelConsonant.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Alphabet");
+        char ch = sc.next().charAt(0);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+            System.out.println( ch+ " Alphabet  is Vowel");
+        }else{
+            System.out.println( ch+ " Alphabet is Consonant");
+        }
+    }
+}
