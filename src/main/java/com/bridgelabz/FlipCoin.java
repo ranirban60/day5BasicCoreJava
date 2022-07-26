59703fd73ece09cc827cb39d50a869dcb3ccb3bb
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-26 11:10:38 +0530

    Added Program Flip a Coin

---
+++ src/main/java/com/bridgelabz/FlipCoin.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class FlipCoin {
+    public static void main(String[] args) {
+
+            if (Math.random() < 0.5) {
+                System.out.println("Head");
+            } else {
+                System.out.println("Tail");
+            }
+        }
+    }
+
